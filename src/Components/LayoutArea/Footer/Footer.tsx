ce339f22ff07aa6d7d54eba296a29346991812d2
rikimarmorstein <rikim1010@gmail.com>
2023-05-05 15:47:16 +0300

    update

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -10,7 +10,9 @@ function Footer(): JSX.Element {
             <span>&copy; Riki Marmorstein &nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp; <a href="https://github.com/rikimarmorstein?tab=repositories" target="newwin" title="To my github"><BsGithub className="git" /></a>
                 <a href="https://wa.me/972559339449?text=%D7%9C%D7%A9%D7%9C%D7%99%D7%97%D7%AA%20%D7%94%D7%95%D7%93%D7%A2%D7%94
 " target="newwin" title="Chat on WhatsApp"><BsWhatsapp className="whatsapp" /></a>&nbsp;&nbsp;
-                <a href="mailto:rikim1010@gmail.com?subject=to deer chv&body=my deer i so want to back to you" className="email" title="Send an email" target="newwin"><MdAlternateEmail /></a>
+                <a href="mailto:rikim1010@gmail.com"
+                //</span>?subject="?"" &body="?"
+                 className="email" title="Send an email" target="newwin"><MdAlternateEmail /></a>
 
 
 
